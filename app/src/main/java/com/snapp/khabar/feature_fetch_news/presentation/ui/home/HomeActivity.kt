f08003dd1d9820e5d5c4de1ba52d2836576fa537
Spiderman <rahilmajeed2@gmail.com>
2022-06-02 13:30:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/snapp/khabar/feature_fetch_news/presentation/ui/home/HomeActivity.kt
@@ -0,0 +1,35 @@
+package com.snapp.khabar.feature_fetch_news.presentation.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.snapp.khabar.R
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val fragmentManager = supportFragmentManager
+
+        viewPagerAdapter = ViewPagerAdapter(fragmentManager,lifecycle)
+        val viewpager = findViewById<ViewPager2>(R.id.viewPager)
+        viewpager.adapter = viewPagerAdapter
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        TabLayoutMediator(tabLayout,viewpager){tab , position ->
+            tab.text = when(position){
+                0 -> "All news"
+                1 -> "Business"
+                2 -> "Politics"
+                3 -> "Tech"
+                4 -> "Science"
+                else -> "Sports"
+            }
+
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/snapp/khabar/feature_fetch_news/presentation/ui/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.snapp.khabar.feature_fetch_news.presentation.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.snapp.khabar.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
